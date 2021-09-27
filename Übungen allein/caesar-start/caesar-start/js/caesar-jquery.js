93b3ba16cba4121346fd37c564f2e82272cb61bc
MatthiasZw <84776864+MatthiasZw@users.noreply.github.com>
2021-09-27 15:11:12 +0200

    caesar anfang

--- Übungen allein/caesar-start/caesar-start/js/caesar-jquery.js
+++ Übungen allein/caesar-start/caesar-start/js/caesar-jquery.js
@@ -1,1 +1,26 @@
-/* caesar-jquery.js */
+var eingabe= 0
+
+function encrypt(){
+
+    eingabe= $('#text').val();
+    versch= $('#code').val();
+
+    console.log(eingabe);
+    console.log(versch);
+
+    for(char in eingabe){
+    ausgabe= eingabe.charCodeAt(0);
+    console.log(ausgabe);
+
+    
+    }
+
+}
+
+function init(){
+
+    document.querySelector('#encrypt').onclick = encrypt;
+
+}
+
+document.addEventListener('DOMContentLoaded', init, true);
