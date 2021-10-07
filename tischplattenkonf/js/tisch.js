561a21c003e286c275611d9d5a9024a2ed888a62
MatthiasZw <84776864+MatthiasZw@users.noreply.github.com>
2021-10-07 11:11:36 +0200

    Tischplatte funktioniert

--- tischplattenkonf/js/tisch.js
+++ tischplattenkonf/js/tisch.js
@@ -1,9 +1,67 @@
 $(document).ready(function(){
 
         $('#cal').click(function(){
-            let pr = 0;
-            pr= $('#la').val() * $('#br').val();
-            console.log(pr);
+            let m2 = 0;
+            let m3= 0;
+ 
+            m2= ($('#la').val() * $('#br').val())/10000;
+
+          
+
+            if($('input[name="auf"]:checked').val()== 0 ){
+                m3= m3 + 47.60;
+                }
+            
+            if($('input[name="ri"]:checked').val()== 2 ){
+                m3= m3 + 71.40;
+                }
+
+            let st = $('input[name="staerke"]:checked').val();
+            
+            switch (st)
+               {
+                case '0':
+                    m2=m2*(300 + m3);
+                    break;
+                case '1':
+                    m2=m2*(360 + m3);
+                    break;
+                case '2':
+                    m2=m2*(420 + m3);
+                    break;
+                case '3':
+                    m2=m2*(480 + m3);
+                    break;
+                case '4':
+                    m2=m2*(560 + m3);
+                    break; 
+                case '5':
+                    m2=m2*(640 + m3);
+                    break;   
+                case '6':
+                    m2=m2*(720 + m3);
+                    break; 
+                case '7':
+                    m2=m2*(800 + m3);
+                    break; 
+                case '8':
+                    m2=m2*(880 + m3);
+                    break; 
+                case '9':
+                    m2=m2*(880 + m3);
+                    break;
+                case '10':
+                    m2=m2*(960 + m3);
+                    break;
+                case '11':
+                    m2=m2*(1040 + m3);
+                    break;
+                default:
+                    $('#aus').html('Fehler');
+            }
+
+            $('#aus').html(m2);
+
         });
 
 });
