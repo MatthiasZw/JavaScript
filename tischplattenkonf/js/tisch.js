68bd841a4829369f35ff1ceacdb7bbecf8a8ed6f
MatthiasZw <84776864+MatthiasZw@users.noreply.github.com>
2021-10-07 09:31:49 +0200

    Tsichplatte Struktur

---
+++ tischplattenkonf/js/tisch.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+        $('#cal').click(function(){
+            let pr = 0;
+            pr= $('#la').val() * $('#br').val();
+            console.log(pr);
+        });
+
+});
